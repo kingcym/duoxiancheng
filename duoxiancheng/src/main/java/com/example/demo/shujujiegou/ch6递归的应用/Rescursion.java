f14a002bb465be8a5c2d98f977633ec6bc8313a5
caiyaming <3286929@qq.com>
2017-11-22 21:01:33 +0800

    no message

--- duoxiancheng/src/main/java/com/example/demo/shujujiegou/ch6递归的应用/Rescursion.java
+++ duoxiancheng/src/main/java/com/example/demo/shujujiegou/ch6递归的应用/Rescursion.java
@@ -4,4 +4,17 @@ package com.example.demo.shujujiegou.ch6递归的应用;
  * Created by hasee on 2017/11/21.
  */
 public class Rescursion {
+    public static void main(String[] args) {
+        test(5);
+
+    }
+
+    private static void test(int i) {
+        System.out.println(i);
+        if (i == 0){
+            return;
+        }
+        test(i-1);
+        System.out.println("==="+i);
+    }
 }
